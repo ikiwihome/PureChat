a2a02e44b9cbf8b7ff24adaae3e1870a5d31078c
ikiwi <ikiwicc@gmail.com>
2025-12-22 22:02:18 +0800

    优化AppSidebar和index页面的SVG图标样式，调整按钮类名以提高可读性

--- app/components/AppSidebar.vue
+++ app/components/AppSidebar.vue
@@ -9,7 +9,7 @@
             创建新会话
           </div>
           <span class="flex items-center">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="text-black dark:text-white h-[18px] w-[18px]">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="text-black dark:text-white h-4.5 w-4.5">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M16.7929 2.79289C18.0118 1.57394 19.9882 1.57394 21.2071 2.79289C22.4261 4.01184 22.4261 5.98815 21.2071 7.20711L12.7071 15.7071C12.5196 15.8946 12.2652 16 12 16H9C8.44772 16 8 15.5523 8 15V12C8 11.7348 8.10536 11.4804 8.29289 11.2929L16.7929 2.79289ZM19.7929 4.20711C19.355 3.7692 18.645 3.7692 18.2071 4.2071L10 12.4142V14H11.5858L19.7929 5.79289C20.2308 5.35499 20.2308 4.64501 19.7929 4.20711ZM6 5C5.44772 5 5 5.44771 5 6V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V14C19 13.4477 19.4477 13 20 13C20.5523 13 21 13.4477 21 14V18C21 19.6569 19.6569 21 18 21H6C4.34315 21 3 19.6569 3 18V6C3 4.34314 4.34315 3 6 3H10C10.5523 3 11 3.44771 11 4C11 4.55228 10.5523 5 10 5H6Z" fill="currentColor" />
             </svg>
           </span>
@@ -27,7 +27,7 @@
               <SidebarMenuButton @click="handleSelectSession(session.id)" @mouseenter="hoveredSessionId = session.id" @mouseleave="hoveredSessionId = null" :class="['w-full flex items-center justify-between hover:bg-gray-200/50 dark:hover:bg-gray-200/10',
                 { 'bg-gray-300/50 dark:bg-gray-200/10 rounded-sm': currentSessionId === session.id }]">
                 <img :src="getProviderIcon(session.providerId)" :alt="session.providerId" class="h-4 w-4 dark:filter dark:invert shrink-0 text-gray-500 dark:text-gray-400 mr-2" />
-                <div class="flex-grow overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+                <div class="grow overflow-hidden text-ellipsis whitespace-nowrap text-sm">
                   {{ session.title || '新会话' }}
                 </div>
                 <button v-show="hoveredSessionId === session.id" @click.stop="confirmDeleteSession(session.id)" class="ml-2 p-1 rounded-md hover:bg-red-100 dark:hover:bg-red-800 text-red-500 flex transition-opacity">
